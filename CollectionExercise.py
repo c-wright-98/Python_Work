9f3fe22c398573509507a52e3182b856f08f703e
c-wright-98 <126788507+c-wright-98@users.noreply.github.com>
2023-07-18 12:14:48 +0100

    Added dictionary and author input

---
+++ CollectionExercise.py
@@ -0,0 +1,7 @@
+books = {"Margaret Atwood": ["The Handmaiden's Tale"],
+        "J.R.R. Tolkien": ["The Hobbit"],
+        "Roald Dahl": ["Charlie and the Chocolate Factory"]}
+
+author = input("Enter the author name: ")
+
+
