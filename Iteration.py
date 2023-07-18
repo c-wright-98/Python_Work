67df202c390a0de05ce125c2658793848f5dcd42
c-wright-98 <126788507+c-wright-98@users.noreply.github.com>
2023-07-18 15:29:51 +0100

    For loop

--- Iteration.py
+++ Iteration.py
@@ -1,4 +1,4 @@
-#While Loop
+##While Loop
 
 count = 0
 name = str(input("What is your name: "))
@@ -8,4 +8,11 @@ while count < len(name):
     print(name, "is awesome!")
     count += 1
 
+## For loop
+
+#Prints all the numbers from 5 - 10
+for i in range(5, 11):
+    print(i)
+
+##Execise
 
