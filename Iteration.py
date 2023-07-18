c2b7df2b0e1e5e57198fe2d6835526f3eae1d254
c-wright-98 <126788507+c-wright-98@users.noreply.github.com>
2023-07-18 15:27:41 +0100

    Added while loop

---
+++ Iteration.py
@@ -0,0 +1,11 @@
+#While Loop
+
+count = 0
+name = str(input("What is your name: "))
+
+#wnated to do something fun and made it the length of the name
+while count < len(name):
+    print(name, "is awesome!")
+    count += 1
+
+
