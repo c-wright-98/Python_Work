9fa577c04c395911e3304b0054ce95bb7e4385c7
c-wright-98 <126788507+c-wright-98@users.noreply.github.com>
2023-07-18 11:15:18 +0100

    Completed data types exercise

---
+++ DataTypes.py
@@ -0,0 +1,39 @@
+## Strings
+
+word1 = "Hello "
+word2 = "World!"
+
+HelloWorld = word1 + word2
+
+print(HelloWorld)
+
+## Integers
+
+number1 = input("Enter whole number: ")
+number2 = input("Enter decimal number: ")
+
+integer_number = int(number1)
+float_number = float(number2)
+round_number = int(round(float_number))
+
+print(number1)
+print(number2)
+print(round_number)
+
+
+## Booleans
+
+# Pet
+name = "Pep Guardogiola"
+age = 3
+bark = True
+tweet = False
+
+print("My pet is called", name, ", He is", age, "years old.")
+print("Statement:", name, "barks.", bark)
+print("Statement:", name, "tweets.", tweet)
+
+# This piece of code is pritning statments using the variables listed
+# it states the name and the age of the pet in the first stament
+# It then states the pets name along with the string 'barks' and the TRUE statement asssigned to the bark variable. 
+# It then states the pet name along with the string tweet and the False statement asssigned to the Tweet variable.
