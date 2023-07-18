ac779747925b01228530e5d82037dd17c1164e6c
c-wright-98 <126788507+c-wright-98@users.noreply.github.com>
2023-07-18 15:33:54 +0100

    Explained for loop iteration

--- IterationExercise.py
+++ IterationExercise.py
@@ -1,1 +1,12 @@
 #Create while loop to print name and 'is awesome!' 5 times
+
+count = 0
+
+while count < 5:
+    name = input("Please enter a name: ")
+    print(name + " is awesome!")
+    count += 1
+
+#Explain while loop
+for i in range(10, 21, 2):
+    print(i)
