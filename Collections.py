0853a601c4f10e10906282c29af32c590dd10587
c-wright-98 <126788507+c-wright-98@users.noreply.github.com>
2023-07-18 12:12:59 +0100

    Collection Tutorial

---
+++ Collections.py
@@ -0,0 +1,6 @@
+books = {"The Handmaiden's Tale":"Margaret Atwood", "The Hobbit":"J.R.R. Tolkien", "Charlie and the Chocolate Factory":"Roald Dahl"}
+
+print(books["The Handmaiden's Tale"]) # print associated key value
+
+print(books["Margaret Atwood"]) # This throws an error as the author's name is a value not a key
+
