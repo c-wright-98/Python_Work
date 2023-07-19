696810a8078f8afb658c25f254d4da9dbca38dc4
c-wright-98 <126788507+c-wright-98@users.noreply.github.com>
2023-07-19 10:57:42 +0100

    Used function printed statements

--- Functions/FunctionExercise.py
+++ Functions/FunctionExercise.py
@@ -5,12 +5,16 @@ def calculate_grade(name, homework_score, assessment_score, final_score):
 
     return name, percentage
 
-# Take Inputs
+# Take Inputs and assign to variables
 
 name = str(input("Enter student's name: "))
 homework = int(input("Enter homework score (/25): "))
 assessment = int(input("Enter assessment score (/50): "))
 final_exam = int(input("Enter final exam score (/100): "))
 
+#Assign name and percentage to variables using function
+student_name, grade_percentage = calculate_grade(name, homework, assessment, final_exam)
 
+print(f"Student Name: {student_name}")
+print(f"Grade Percentage: {grade_percentage}%")
 
