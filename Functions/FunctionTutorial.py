172a6ddda9f1119d153d1b419ceff6bb300824f2
c-wright-98 <126788507+c-wright-98@users.noreply.github.com>
2023-07-19 10:20:07 +0100

    Completed tutorial

---
+++ Functions/FunctionTutorial.py
@@ -0,0 +1,8 @@
+# tutorial shows how to make a simple function that adds the two numbers passed in the arguments returning the added numbers
+
+def add_calc(number1, number2):
+    answer = number1 + number2
+    return answer
+
+added_number = add_calc(5,5)
+print(added_number + 20)
