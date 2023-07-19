ab06eb4818498836ec2d22026146f95d4455a51f
c-wright-98 <126788507+c-wright-98@users.noreply.github.com>
2023-07-19 15:09:21 +0100

    Fixed Exercise 4

--- Debugging/Exercise3.py
+++ Debugging/Exercise3.py
@@ -7,5 +7,14 @@ def is_prime(x):
                 return False
             return True
 
-test = is_prime(2, 3, 4, 5, 6, 7, 15, 20, 25)
-print(test)
+test1 = is_prime(2)
+test2 = is_prime(3)
+test3 = is_prime(4)
+test4 = is_prime(5)
+test5 = is_prime(6)
+test6 = is_prime(7)
+test7 = is_prime(15)
+test8 = is_prime(20)
+test9 = is_prime(25)
+
+print(test1,test2,test3,test4,test5,test6,test7,test8,test9)
--- Debugging/Exercise4.py
+++ Debugging/Exercise4.py
@@ -3,6 +3,7 @@ n = 0
 
 while n < 5:
     for i in item_list:
-        print(item_list[i])
+        print(i)
+    n += 1
 
 print (item_list[5])
