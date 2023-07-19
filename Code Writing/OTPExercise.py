5c50e34122846f2508ab760ebb3c9d79590a2543
c-wright-98 <126788507+c-wright-98@users.noreply.github.com>
2023-07-19 16:29:35 +0100

    Added OTP generator

--- Code Writing/OTPExercise.py
+++ Code Writing/OTPExercise.py
@@ -1,3 +1,10 @@
 # Using the secrets module to generate six digit OTP
 
+import secrets as sc
 
+generator = sc.SystemsRandom()
+
+#generate the six digit code, can't use 000000 as python won't register it nor would it 0000001
+otp = generator.randrange(100000, 999999)
+
+print(f"Your OTP is: {otp}")
