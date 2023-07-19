b272016067e71bdf0bb2a7aed4fb1320cdcd0295
c-wright-98 <126788507+c-wright-98@users.noreply.github.com>
2023-07-19 10:26:27 +0100

    Added total score and percentage calculator

--- Functions/FunctionExercise.py
+++ Functions/FunctionExercise.py
@@ -1,1 +1,5 @@
-def calculate_grade(name, homework_score, assessemnt_score, final_score):
+def calculate_grade(name, homework_score, assessment_score, final_score):
+    total_score = homework_score + assessment_score + final_score
+    percentage = (total_score/175)*100
+    #175 comes from the total possbile score from each aargument (25+50+100 = 175)
+    
