6ca7c10810bbbe08e7f0f7de31924870abad6ccf
c-wright-98 <126788507+c-wright-98@users.noreply.github.com>
2023-07-19 12:23:52 +0100

    Imported dice, rolled dice, printed values

---
+++ Modules/Exercise/main.py
@@ -0,0 +1,9 @@
+import dice
+
+# Simulating throwing 2 dice
+dice1 = dice.roll_dice()
+dice2 = dice.roll_dice()
+
+# Printing the values of the dice
+print(f"First dice: {dice1}")
+print(f"Second dice: {dice2}")
