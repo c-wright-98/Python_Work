214a70af4eb4b978b9c1146ddb6f8952847b7c99
c-wright-98 <126788507+c-wright-98@users.noreply.github.com>
2023-07-20 11:32:11 +0100

    Created show balance function

--- Classes/BudgetApp.py
+++ Classes/BudgetApp.py
@@ -11,18 +11,27 @@ class budget:
     def add_category(self, category_name, initial_balance = 0):
         if category_name not in self.catergories:
             self.catergories[category_name] = initial_balance
+            print(f"{category_name} created")
 
     def deposit(self, category_name, amount):
         if category_name in self.catergories:
             self.catergories[category_name] += amount
+            print(f" Successfully deposited £{amount} for {category_name}")
 
     def withdraw(self, category_name, amount):
         if category_name in self.catergories:
             if self.catergories[category_name] >= amount:
                 self.catergories[category_name] -= amount
+                print(f" Successfully withdrew £{amount} from {category_name}")
             else:
                 print("Insufficent Funds")
-    
+
+    def show_balance(self, category_name):
+        if category_name in self.categories:
+            print(f"There is £{self.categories[category_name]} in {category_name}")
+
+
+
 
 
 
