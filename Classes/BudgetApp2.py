1dec77bd30671974a6c27f901539d27f5a66a351
c-wright-98 <126788507+c-wright-98@users.noreply.github.com>
2023-07-20 11:45:13 +0100

    Created budget class

---
+++ Classes/BudgetApp2.py
@@ -0,0 +1,9 @@
+# Goal: “Create a Budget class that can instantiate objects based on different budget categories
+# like food, clothing, and entertainment.
+# These objects should allow for depositing and withdrawing funds from each category,
+# as well computing category balances and transferring balance amounts between categories”
+
+class budget:
+    def __init__(self, category, balance = 0):
+        self.category = category
+        self.balance = balance
