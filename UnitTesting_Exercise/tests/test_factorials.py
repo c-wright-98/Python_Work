99f6f7baa226cbf8a1c22b69a504d4a0764521cd
c-wright-98 <corvus.wright@gmail.com>
2023-07-21 11:46:32 +0100

    Added test to factorial and successfully tested

--- UnitTesting_Exercise/tests/test_factorials.py
+++ UnitTesting_Exercise/tests/test_factorials.py
@@ -1,4 +1,9 @@
 import pytest
 from programs import factorial
 
-#def
+def test_fact_zero():
+    assert factorial.fact(0) == 1
+
+def test_fact_post():
+    assert factorial.fact(1) == 1
+    assert factorial.fact(5) == 120
