6a3df67b75663bde7a4e92a7abcab974ed648195
c-wright-98 <corvus.wright@gmail.com>
2023-07-24 12:09:29 +0100

    Added label and printed to gui

--- GUI_Fun/gui_examply.py
+++ GUI_Fun/gui_examply.py
@@ -1,2 +1,13 @@
-import tkinter
+import tkinter as tk
 
+#This initialises tkinter and lets us pass pur visuals onto something
+root = tk.Tk()
+
+label = tk.Label(root,text = "Hello World!")
+label.grid(row = 1,column = 1)
+
+#This object has a main loop object that allows us to interact with it
+# It's good practice to run this in the __main__ so it runs when the file is ran
+
+if __name__ == '__main__':
+    root.mainloop()
