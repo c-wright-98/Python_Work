05ab54e07bee6522951bd50908bcb50ceca08127
c-wright-98 <corvus.wright@gmail.com>
2023-07-24 11:23:11 +0100

    Commented out code

--- Classes/StudentClass.py
+++ Classes/StudentClass.py
@@ -40,11 +40,11 @@ class student:
         print(f"{self.name}'s average test score is: {average_score}")
 
 
-student1 = student('John', 18)
-student2 = student('Emily', 21)
+#student1 = student('John', 18)
+#student2 = student('Emily', 21)
 
-student1.add_test_score()
-student1.get_average_test_score()
+#student1.add_test_score()
+#student1.get_average_test_score()
 
-student2.add_test_score()
-student2.get_average_test_score()
+#student2.add_test_score()
+#student2.get_average_test_score()
