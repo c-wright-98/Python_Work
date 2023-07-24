20dce84c0895af8ef790fbd20463aefbe8144055
c-wright-98 <corvus.wright@gmail.com>
2023-07-24 15:37:43 +0100

    Fixed the int that was init and stopping the class from creating

--- Magic_Methods/magic_methods.py
+++ Magic_Methods/magic_methods.py
@@ -9,11 +9,25 @@ Feel free to play around with implementing any of the other magic methods if you
 '''
 
 class rectangle:
-    def __init__(self, height:int, width:int) -> None:
+    def __init__(self, height, width):
         self.height = height
         self.width = width
 
     def __str__ (self):
-        pass
-    def __init__(self):
-        pass
+        rectangle_str = ''
+        for _ in range(self.height):
+            rectangle_str += "*" * self.width + "\n"
+        return rectangle_str
+
+    def __int__(self):
+        height = self.height
+        width = self.width
+        return height*width
+
+if __name__ == '__main__':
+    Rectangle = rectangle(3,8)
+    Rectangle2 = rectangle(5,10)
+    print(Rectangle)
+    print(int(Rectangle))
+    print(Rectangle2)
+    print(int(Rectangle2))
