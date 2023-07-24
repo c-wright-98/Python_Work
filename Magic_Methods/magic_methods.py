07c7b97daaaabd77b1562b08809ec52829957d70
c-wright-98 <corvus.wright@gmail.com>
2023-07-24 14:55:01 +0100

    Added instructions

---
+++ Magic_Methods/magic_methods.py
@@ -0,0 +1,10 @@
+'''
+create a rectangle class which implements:
+
+an __init__ method which sets the rectangles height and width
+a __str__ method which returns a string which is an ascii-art drawing of the rectangle
+an __int__ method which returns the rectangle's area
+
+Feel free to play around with implementing any of the other magic methods if you want to
+'''
+
